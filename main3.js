c4675b5efbfe1157dc044192bc54edf2eda672ce
RbYy <robert.brumat@gmail.com>
2016-08-25 21:00:55 +0200

    deluje ...

---
+++ main3.js
@@ -0,0 +1,28 @@
+var app = angular.module("app", []);
+
+app.controller('kontrola', ['$scope', function($scope){
+    $scope.start = function(){
+        $scope.besede = $scope.besedilo.split(" ")
+    }
+}])
+
+app.factory('servisPobarvaj', function(){
+    ser = function(scope, element, attr){
+        scope.barva = "black"
+        scope.pobarvaj = function(stBesede){ //zaporedna stevilka besede
+            scope.stBesede =stBesede
+            scope.nova=attr.barva
+        }
+    }
+    return ser
+})
+
+app.directive('besede',['servisPobarvaj', function(servisPobarvaj){
+    return{
+        restrict: "E",
+        templateUrl: 'templ2.html',
+        replace:true,
+        link: servisPobarvaj
+
+    }
+}])
