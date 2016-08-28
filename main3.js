10fe289177f372f4e551a699fc38ed25fa2c4ca5
RbYy <robert.brumat@gmail.com>
2016-08-28 10:58:49 +0200

    vpelji isolated scope in , odstrani 'submit'

--- main3.js
+++ main3.js
@@ -1,14 +1,16 @@
 var app = angular.module("app", []);
 
 app.controller('kontrola', ['$scope', function($scope){
-    $scope.start = function(){
-        $scope.besede = $scope.besedilo.split(" ")
-    }
+
 }])
 
 app.factory('servisPobarvaj', function(){
     ser = function(scope, element, attr){
+        scope.$watch('besedilo',function(){
+            scope.parsano = scope.besedilo.split(" ")
+        })
         scope.barva = "black"
+        
         scope.pobarvaj = function(stBesede){ //zaporedna stevilka besede
             scope.stBesede =stBesede
             scope.nova=attr.barva
@@ -21,7 +23,11 @@ app.directive('besede',['servisPobarvaj', function(servisPobarvaj){
     return{
         restrict: "E",
         templateUrl: 'templ2.html',
-        replace:true,
+        replace: true,
+        scope: {
+            barva: '@',
+            besedilo: '@'
+        },
         link: servisPobarvaj // namesto, da bi definiral anonimno funkcijo,
 							// sem registriral service in ga injektiral v direktivo
     }
